ead602b6b080a89a556b8c20b64842495dd369ca
kuzyakAAA <artem-kuzmin-flash@yandex.ru>
2025-10-22 22:48:33 +0300

    2dz

--- if.go
+++ if.go
@@ -3,27 +3,36 @@ package main
 import "fmt"
 
 func Function2() {
-	var num int
+	var num1 int
 	fmt.Println("Введи")
-	fmt.Scan(&num)
-	var fl bool
-	for num < 12307 {
-		if num%9==0 && num%13==0 {
-			fl=false
-			break
-		} else if num < 0 {
-			num *= -1
-		} else if num%7 == 0 {
-			num *= 39
-		} else if num&9 == 0 {
-			num = num*13 + 1
-		} else {
-			num = (num + 2) * 3
+	fmt.Scan(&num1)
+
+	var fl bool = true
+	var num = num1
+
+	if num1 <= 12307 {
+		for num < 12307 {
+			if num%9 == 0 && num%13 == 0 && num != 0 {
+				fl = false
+				break
+			} else if num < 0 {
+				num *= -1
+			} else if num%7 == 0 && num != 0 {
+				num *= 39
+			} else if num%9 == 0 && num != 0 {
+				num = num*13 + 1
+			} else {
+				num = (num + 2) * 3
+			}
 		}
-	}
-	if !fl{
+
+		if !fl {
 			fmt.Println("server error")
-	} else {
+		} else {
 			fmt.Println(num)
+		}
+
+	} else {
+		fmt.Println("Число", num1, "уже больше числа 12307")
 	}
 }
